32e0244b47b8ed164329eacb052460617d1720cb
Victoria <olumideronke@gmail.com>
2022-07-21 03:44:19 -0700

    Sign

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
-import About from './pages/About';
-import Footer from './components/Footer';
+// import About from './pages/About';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+// import Footer from './components/Footer';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <About />
-      <Footer />
+      {/* <About /> */}
+      <SignIn/>
+      <SignUp/>
+      {/* <Footer /> */}
     </div>
   )
 }
---
+++ src/pages/SignUp.js
@@ -0,0 +1,274 @@
+import React from 'react';
+import styled from 'styled-components';
+import Logo from '../assets/images/Logo.png';
+import Fine from '../assets/images/fine.png';
+import Google from '../assets/images/google.png';
+import Github from '../assets/images/git.png';
+import Question from '../assets/images/Q.png';
+
+
+
+const SignUp = () => {
+  return (
+    <SignInStyle>
+        <div>
+            <div className='logo-container' style={{margin: '1rem 0'}}>
+                <img src={Logo} alt="Logo"  style={{width: '3.5rem', height: '3.5rem'}}/>
+            </div>
+            <div>
+                <h2>Join Talsgrow</h2>
+                <div className='sign-flex'>
+                    <FormStyleLeft>
+                        <div className='form-cover'>
+                            <form action="">
+                                <div className='input-cover'>
+                                <label htmlFor="email">Full Name</label>
+                                <input type="text" name='full name' id='fullname' placeholder='Felix Simon'/>
+                            </div>
+                            <div className='input-cover'>
+                                <label htmlFor="email">Email Address</label>
+                                <input type="text" name='email' id='email' placeholder='example247@gmail.com'/>
+                            </div>
+                            <div className='input-cover'>
+                                <label htmlFor="password">Password</label>
+                                <input type="password" name='password' id='password' placeholder='.....'/>
+                            </div>
+                            <div className='check-input-cover'>
+                                <div className='check-input'>
+                                    <input type="checkbox" name='checkbox'/>
+                                    <label htmlFor="checkbox">Remember me</label>
+                                </div>
+                                <span>Forgot password?</span>
+                            </div>
+                            <ButtonStyle><p>Sign Up</p></ButtonStyle>
+                            <p>Want to become a member? <span>Join now</span></p>
+                            </form>
+                        </div>
+                    </FormStyleLeft>
+                    <SignInLeft>
+                        <div className='signIn-cards'>
+                        <div>
+                            <ButtonStyle style={{backgroundColor: '#FFF', color: '#0950CD'}}><span><img src={Google} alt="google-logo" /></span> <p>Login With Google</p></ButtonStyle>
+                            <ButtonStyle><span><img src={Github} alt="google-logo" /></span> <p>Login With Github</p></ButtonStyle>
+                        </div>
+                        </div>
+                    </SignInLeft>
+                </div>
+            </div>
+            <div className='question-sect'>
+                <img src={Question} alt="question" />
+            </div>
+        </div>
+    </SignInStyle>
+  )
+}
+
+const SignInStyle = styled.div`
+height: 100%;
+.logo-container{
+    margin:0;
+    padding: 1rem;
+    border-bottom: 1px solid lightblue;
+}
+.sign-flex{
+    display:flex;
+    justify-content: space-around;
+    gap: 10px;
+    
+    label{
+        display: block;
+    }
+}
+h2{
+    margin: 2rem;
+    font-size: 2rem;
+}
+p, label, span{
+    font-family: 'Ubuntu', sans-serif;
+}
+.question-sect{
+    display: flex;
+    justify-content: flex-end;
+    img{
+        margin:0 .5rem;
+        width: 30px;
+        height: 30px;
+    }
+}
+
+@media screen and (max-width: 1040px){
+    .sign-flex{
+        display: flex;
+        flex-direction: column-reverse;
+        margin: 2rem;
+    }
+    
+}
+
+`
+const FormStyleLeft = styled.div`
+    width: 35%;
+    display: flex;
+    justify-content: center;
+    .form-cover{
+        width: 100%;
+    }
+    
+    .input-cover{
+        width: 100%;
+        position: relative;
+        label{
+            position: absolute;
+            top: 20px;
+            left: 25px;
+            background-color: #FFF;
+        }
+    }
+    
+    #email, #password, #fullname{
+        border-radius: 50px;
+        height: 55px;
+        margin: 2rem 0;
+        font-size: 1rem;
+        width: 92%;
+        border: 1px solid #0950CD;
+        padding:0 1.5rem;
+    }
+    #email:focus, #password:focus, #fullname:focus{
+        outline: 2px solid #0950CD;
+    }
+    .check-input-cover{
+        display: flex;
+        justify-content: space-between;
+        .check-input{
+            display:flex;
+            input{
+                width: 20px;
+                height: 20px;
+            }
+        }
+    }
+    span{
+        color: red;
+    }
+    p{
+        text-align:center;
+    }
+    p, label, span{
+        font-size: 1.2rem;
+    }
+    @media screen and (max-width: 1040px){
+        width: 100%;
+    }
+    @media screen and (max-width: 680px){
+        p, label, span{
+            font-size: 1rem;
+        }
+        #email, #password, #fullname{
+            padding: 0 1rem;
+            font-size: .8rem;
+        }
+    }
+`
+const ButtonStyle = styled.button`
+        padding: 1rem 2rem;
+        border-radius: 50px;
+        margin: 1rem 0;
+        font-size: 1rem;
+        width: 100%;
+        background-color: #0950CD;
+        border: none;
+        color: #FFF;
+        cursor: pointer;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        gap: 2rem;
+        p{
+            margin: 0;
+        }
+        span{
+            background-color: #FFF;
+            height: 30px;
+            width: 30px;
+            padding: .2rem;
+            border-radius: 50%;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            img{
+            width: 25px;
+            height: 25px;
+            }
+        }
+        @media screen and (max-width: 680px){
+            font-size: .6rem;
+            gap: 1rem;
+            padding: 1rem 2rem;
+            
+            span{
+                width: 20px;
+                height: 20px;
+                img{
+                    width: 18px;
+                    height: 18px;
+                }
+            }
+            p{
+                font-size: .8rem;
+            }
+        
+        }
+`
+const SignInLeft = styled.div`
+    color: #fff;
+    width: 40%;
+    height: 600px;
+    border-radius: 25px 40px 25px 40px;
+    box-sizing: border-box;
+    margin: 2rem 0;
+    .signIn-cards{
+        width: 100%;
+        height: 100%;
+        background-image: url(${Fine});
+        background-size: cover;
+        background-repeat: no-repeat;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        gap:0;
+        div{
+            width: 80%;
+        }
+        
+    }
+
+    @media screen and (max-width: 1040px){
+        width: 100%;
+        display: flex;
+        justify-content: center;
+        .signIn-cards{
+            width: 100%;
+        }
+        
+    }
+    @media screen and (max-width: 680px){
+        height: 400px;
+    }
+    @media screen and (max-width: 360px){
+        height: 300px;
+        .signIn-cards{
+            width: 90%;
+        }
+        .signIn-tags{
+            width:100px;
+            padding: .2rem .5rem;
+            p{
+                font-size: .8rem;
+            }
+        }
+        
+    }
+    
+`
+export default SignUp
